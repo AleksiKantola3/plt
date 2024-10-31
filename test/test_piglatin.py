8896ad9518d02848efd27b366fbea382abacbd5e
AleKantola <aleksi3472@gmail.com>
2024-10-31 17:03:26 +0200

    Test Case 1 green

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -1,9 +1,13 @@
 import unittest
+
+import piglatin
 from piglatin import PigLatin
 from error import PigLatinError
 
 
 class TestPigLatin(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_input_phrase(self):
+        phrase = "hello world"
+        translator = PigLatin(phrase)
+        self.assertEqual(phrase, translator.get_phrase())
